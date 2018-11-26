8533ff6b35febc9faf16dc7a2197b2b489f86b71
abhijeet3922 <abhijeetchar@gmail.com>
2018-11-27 03:10:59 +0530

    adding expression detection on single image

---
+++ facial_emotion_image.py
@@ -0,0 +1,52 @@
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+import imutils
+import cv2
+import numpy as np
+import sys
+
+# parameters for loading data and images
+detection_model_path = 'haarcascade_files/haarcascade_frontalface_default.xml'
+emotion_model_path = 'models/_mini_XCEPTION.106-0.65.hdf5'
+img_path = sys.argv[1]
+
+# hyper-parameters for bounding boxes shape
+# loading models
+face_detection = cv2.CascadeClassifier(detection_model_path)
+emotion_classifier = load_model(emotion_model_path, compile=False)
+EMOTIONS = ["angry","disgust","scared", "happy", "sad", "surprised","neutral"]
+
+
+#reading the frame
+orig_frame = cv2.imread(img_path) 
+frame = cv2.imread(img_path,0)
+#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+faces = face_detection.detectMultiScale(frame,scaleFactor=1.1,minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
+
+if len(faces) > 0:
+	faces = sorted(faces, reverse=True,key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+	(fX, fY, fW, fH) = faces
+	roi = frame[fY:fY + fH, fX:fX + fW]
+	roi = cv2.resize(roi, (48, 48))
+	roi = roi.astype("float") / 255.0
+	roi = img_to_array(roi)
+	roi = np.expand_dims(roi, axis=0)
+	preds = emotion_classifier.predict(roi)[0]
+	emotion_probability = np.max(preds)
+	label = EMOTIONS[preds.argmax()]
+
+
+for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, preds)):
+        # construct the label text
+        text = "{}: {:.2f}%".format(emotion, prob * 100)
+        cv2.putText(orig_frame, label, (fX, fY - 10),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        cv2.rectangle(orig_frame, (fX, fY), (fX + fW, fY + fH),(0, 0, 255), 2)
+        print(text)
+
+cv2.imshow('test_face', orig_frame)
+cv2.imwrite('test_output/'+img_path.split('/')[-1],orig_frame)
+if (cv2.waitKey(2000) & 0xFF == ord('q')):
+    sys.exit("Thanks")
+cv2.destroyAllWindows()
+
