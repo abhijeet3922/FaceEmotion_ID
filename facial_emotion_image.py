54273b41cdc60c9be4f3a1af294ca30d6d1138d2
abhijeet3922 <abhijeetchar@gmail.com>
2018-11-27 06:41:19 +0530

    updating code for testing single image

--- facial_emotion_image.py
+++ facial_emotion_image.py
@@ -20,30 +20,22 @@ EMOTIONS = ["angry","disgust","scared", "happy", "sad", "surprised","neutral"]
 #reading the frame
 orig_frame = cv2.imread(img_path) 
 frame = cv2.imread(img_path,0)
-#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 faces = face_detection.detectMultiScale(frame,scaleFactor=1.1,minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
 
 if len(faces) > 0:
-	faces = sorted(faces, reverse=True,key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
-	(fX, fY, fW, fH) = faces
-	roi = frame[fY:fY + fH, fX:fX + fW]
-	roi = cv2.resize(roi, (48, 48))
-	roi = roi.astype("float") / 255.0
-	roi = img_to_array(roi)
-	roi = np.expand_dims(roi, axis=0)
-	preds = emotion_classifier.predict(roi)[0]
-	emotion_probability = np.max(preds)
-	label = EMOTIONS[preds.argmax()]
-
-
-for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, preds)):
-        # construct the label text
-        text = "{}: {:.2f}%".format(emotion, prob * 100)
-        cv2.putText(orig_frame, label, (fX, fY - 10),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-        cv2.rectangle(orig_frame, (fX, fY), (fX + fW, fY + fH),(0, 0, 255), 2)
-        print(text)
-
+    faces = sorted(faces, reverse=True,key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+    (fX, fY, fW, fH) = faces
+    roi = frame[fY:fY + fH, fX:fX + fW]
+    roi = cv2.resize(roi, (48, 48))
+    roi = roi.astype("float") / 255.0
+    roi = img_to_array(roi)
+    roi = np.expand_dims(roi, axis=0)
+    preds = emotion_classifier.predict(roi)[0]
+    emotion_probability = np.max(preds)
+    label = EMOTIONS[preds.argmax()]
+    cv2.putText(orig_frame, label, (fX, fY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+    cv2.rectangle(orig_frame, (fX, fY), (fX + fW, fY + fH),(0, 0, 255), 2)
+    
 cv2.imshow('test_face', orig_frame)
 cv2.imwrite('test_output/'+img_path.split('/')[-1],orig_frame)
 if (cv2.waitKey(2000) & 0xFF == ord('q')):
